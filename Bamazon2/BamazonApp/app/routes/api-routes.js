48612cac0b4b1afa0ae9b4e0504967f632784b57
ZintisMay <zbmayk@gmail.com>
2016-06-29 02:52:37 -0400

    working Bamazon

--- Bamazon2/BamazonApp/app/routes/api-routes.js
+++ Bamazon2/BamazonApp/app/routes/api-routes.js
@@ -14,4 +14,32 @@ module.exports = function(app){
 		});
 	});
 
+	app.get('/new', function(req, res){
+
+		console.log(req.body);
+		var newEntry = req.body;
+
+		console.log("/new");
+
+		Bamazon.create({
+			ProductName: newEntry.Name,
+			DepartmentName: newEntry.Department,
+			Price: newEntry.Price,
+			StockQuantity: newEntry.Stock
+		});
+
+		Bamazon.findAll({}).then(function(result){
+			res.json(result);
+		});
+		
+	});
+
+	app.post('/delete', function(req, res){
+		console.log(req.body.idNum);
+		Bamazon.destroy({
+			where:{
+				ItemId: req.body.idNum
+			}
+		});
+	});
 }
